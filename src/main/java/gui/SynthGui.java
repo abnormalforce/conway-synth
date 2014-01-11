1305600b529bd1d270c9f24b81fb36ce599b1817
Patrick McKenna <patrick@abnormalforce.com>
2014-01-11 16:38:58 -0500

    Initial GUI functionality

--- src/main/java/gui/SynthGui.java
+++ src/main/java/gui/SynthGui.java
@@ -1,5 +1,46 @@
 package main.java.gui;
 
-public class SynthGui {
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+import main.java.gui.elements.Cell;
+
+public class SynthGui {
+	JFrame frame = new JFrame();
+	JButton[][] grid;
+	
+	private static final int NUM_COLUMNS = 8;
+	private static final int NUM_NOTES = 8;
+	
+	public static void main(String[] args){
+		new SynthGui();
+	}
+	
+	public SynthGui(){
+		frame.setLayout(new GridLayout(NUM_COLUMNS, NUM_NOTES));
+		grid = new JButton[NUM_COLUMNS][NUM_NOTES];
+		
+		for(int y = 0; y < NUM_NOTES; y++){
+			for(int x = 0; x < NUM_COLUMNS; x++){
+				Cell newCell = new Cell();
+				newCell.addActionListener(new ActionListener() {
+					
+					@Override
+					public void actionPerformed(ActionEvent click) {
+						((Cell)click.getSource()).update();
+					}
+				});
+				grid[x][y] = newCell;
+				frame.add(newCell);
+			}
+		}
+		
+		frame.pack();
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 }
--- src/main/java/gui/elements/Cell.java
+++ src/main/java/gui/elements/Cell.java
@@ -1,7 +1,40 @@
 package main.java.gui.elements;
 
+import java.awt.Color;
+
 import javax.swing.JButton;
 
 public class Cell extends JButton{
-
+	private static final Color aliveColor = Color.CYAN;
+	private static final Color deadColor = Color.BLACK;
+	
+	private Color color;	
+	private boolean isAlive;
+	
+	public Cell(){
+		super(" ");
+		isAlive=false;
+		color = Color.BLACK;
+		setBackground(color);
+	}
+	
+	public void update(){
+		if(isAlive){
+			kill();
+		}
+		else{
+			live();
+		}
+		setBackground(color);
+	}
+	
+	public void live(){
+		isAlive = true;
+		color = aliveColor;
+	}
+	
+	public void kill(){
+		isAlive = false;
+		color = deadColor;
+	}
 }
